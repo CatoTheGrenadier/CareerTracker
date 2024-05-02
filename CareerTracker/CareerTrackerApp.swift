d054051064a0acb86078ca90466dfeee6dafeff0
Cato the Chevalier <131333594+CatoTheGrenadier@users.noreply.github.com>
2024-05-02 09:46:50 -0400

    Initial Commit

---
+++ CareerTracker/CareerTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareerTrackerApp.swift
+//  CareerTracker
+//
+//  Created by Yi Ling on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CareerTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
